ab542af9fc4e323c3d41be05eb25b4d092ee7fdb
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-27 18:59:04 +0100

    class set up

---
+++ models/rock_paper_scissors.rb
@@ -0,0 +1,27 @@
+class RockPaperScissors
+
+  def initialize(move1, move2)
+    @move1 = move1
+    @move2 = move2
+  end
+
+  def game()
+    puts "Player 1: rock, paper, scissors?"
+    @move1 = gets.chomp
+
+    puts "Player 2: rock, paper, scissors?"
+    @move2 = gets.chomp
+
+    if (@move1 == "rock" && @move2 == "paper") || (@move1 == "paper" && @move2 == "rock")
+      puts "Paper wins!"
+    elsif
+      (@move1 == "rock" && @move2 == "scissors") || (@move1 == "scissors" && @move2 == "rock")
+      puts "Rock wins!"
+    elsif
+      (@move1 == "paper" && @move2 == "scissors") || (@move1 == "scissors" && @move2 == "paper")
+      puts "Scissors win!"
+    else
+      puts "It's a draw!"
+  end
+
+end
